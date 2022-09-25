069ca3077d4953efd8e9a600784b66cfdc8d4b8d
Thiều Trần Cương <103253693+cuongthieu1606@users.noreply.github.com>
2022-09-25 21:01:29 +0700

    c# basic

---
+++ CS03.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace CS03
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // + - ^ / %
+            int a = 5;
+            int b = 4;
+            int kq = a + b;
+
+            Console.WriteLine("a + b =" + kq);
+            // Tương tự
+
+
+            // Gán: =, +=, -=, *=, %=, %=
+            int x = 10;
+            x += 2; // x = x + 2 <=> x = 10 + 2 = 12
+            Console.WriteLine(x);
+            // Tương tự -=, *=, %=, %= 
+
+            // ++, --
+            int y = 10;
+
+            int z = 2 * y++;
+            // 2 * x
+            // x = x + 1
+            Console.WriteLine(z);
+
+            int k = 10;
+
+            int i = 2 * ++k;
+            // k = k + 1;
+            // 2 * k
+            Console.WriteLine(i);
+
+
+        }
+    }
+}
